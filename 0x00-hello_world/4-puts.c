9f81f59f2c13217a00840cf9bbb355b1a3ed84f4
moamen-ihab08 <146095726+moamen-ihab08@users.noreply.github.com>
2023-10-06 02:16:02 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - this is the main put function
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
