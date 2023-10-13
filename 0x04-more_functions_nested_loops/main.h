6ff219d091a671588cbe294a631ff6969a897f3b
moamen-ihab08 <146095726+moamen-ihab08@users.noreply.github.com>
2023-10-13 14:20:44 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,20 +10,3 @@ void print_square(int size);
 void print_triangle(int size);
 int mul(int a, int b);
 void print_number(int n);
-
-
-_putchar.c
-
-#include <unistd.h>
-
-/**
-   * _putchar - writes the character c to stdout
-    * @c: The character to print
-     *
-      * Return: On success 1.
-       * On error, -1 is returned, and errno is set appropriately.
-        */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
