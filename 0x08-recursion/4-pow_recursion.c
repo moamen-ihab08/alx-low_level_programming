7271e7bc86e3e1f21e9a54698f8496d52dbccb25
moamen-ihab08 <146095726+moamen-ihab08@users.noreply.github.com>
2023-10-27 00:42:22 +0200

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,8 +5,6 @@
   * @y: parameter 2
   * Return: value of a given number passed to the function
   */
-
-
 int _pow_recursion(int x, int y)
 {
 
@@ -17,8 +15,6 @@ else if (y == 0)
         return (1);
 
 else
-        return (x * _pow_recursion(x, (y - 1)));
-
+    return (x * _pow_recursion(x, (y - 1)));
 return (0);
 }
-
