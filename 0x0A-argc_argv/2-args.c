8aaa83fede297b6954cabe0d87b0a7ea4031ee90
moamen-ihab08 <146095726+moamen-ihab08@users.noreply.github.com>
2023-10-30 15:18:08 +0200

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -16,23 +16,4 @@ int main(int argc, char *argv[])
 		printf("%s\n", argv[i]);
 
 	return (0);
-#include <stdio.h>
-#include "holberton.h"
-
-/**
- * main - Prints all arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
- */
-
-int main(int argc, char *argv[])
-{
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
-	return (0);
-}
 }
